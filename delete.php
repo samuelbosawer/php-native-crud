2ccf3f0892eab5eaad19a29a20125137b26a410d
Samuel Bosawer <zam.bosawer@gmail.com>
2022-06-20 20:20:57 +0900

    Final

---
+++ delete.php
@@ -0,0 +1,15 @@
+<?php
+// include database connection file
+include_once("config.php");
+ 
+// Get id from URL to delete that user
+$id = $_GET['id'];
+ 
+// Delete user row from table based on given id
+$result = mysqli_query($mysqli, "DELETE FROM users WHERE id=$id");
+ 
+// After delete redirect to Home, so that latest user list will be displayed.
+header("Location:index.php");
+?>
+
+File ** delete.php** hanya dipanggil saat kita klik link 'Delete' untuk pengguna manapun yang dipilih.
